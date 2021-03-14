8929ec5e5396df1e34d4af497c32af65072237c8
Trent Tobler <trenttobler@gmail.com>
2021-03-14 08:38:39 -0400

    Added input sample

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,19 +1,31 @@
+import java.util.Scanner;
 
 class HelloWorld {
-	static void Hello() {
+	static void Hello(String name) {
 		hi.Greeter greeter = new hi.Greeter();
-		String greeting = greeter.greet("World");
+		String greeting = greeter.greet(name);
 		System.out.println(greeting);
 	}
 
-	static void Goodbye() {
+	static void Goodbye(String name) {
 		bye.Greeter greeter = new bye.Greeter();
-		String greeting = greeter.greet("World");
+		String greeting = greeter.greet(name);
 		System.out.println(greeting);
 	}
 
 	public static void main(String[] args) {
-		Hello();
-		Goodbye();
+		System.out.print("What is your name? ");
+		Scanner scanner = new Scanner(System.in);
+		if(scanner.hasNextLine())
+		{
+			String name = scanner.nextLine();
+			Hello(name);
+			Goodbye(name);
+		}
+		else
+		{
+			System.out.println("I don't know your name then.");
+		}
+		scanner.close();
 	}
 }
